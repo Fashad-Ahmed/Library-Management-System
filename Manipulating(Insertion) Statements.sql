8c81270e19771ddceaa3c307280c7dcfe22113ba
Fashad Ahmed <fashad.ahmed20@gmail.com>
2021-08-10 19:28:19 +0500

    Project File -Few Insertions just to be sampled ✔

---
+++ Manipulating(Insertion) Statements.sql
@@ -0,0 +1,31 @@
+INSERT 
+INTO book 
+VALUE(110,268,10003,"Duckly Inheritance");
+
+INSERT 
+INTO branch 
+VALUE(6,269,"BLOCK-20,F-SITE");
+
+ INSERT 
+ INTO branch 
+ VALUE(7,270,"BLOCK-20,G-SITE");
+ 
+ INSERT 
+ INTO branch 
+ VALUE(9,272,"BLOCK-21,I-SITE");
+ 
+ INSERT 
+ INTO branch 
+ VALUE(10,273,"BLOCK-23,J-SITE");
+ 
+ INSERT 
+ INTO publisher 
+ VALUE(6660,"Science",5000,1996);
+ 
+ INSERT 
+ INTO author 
+ VALUE("Nazi Hitler", "Robin Scpahnzky");
+ 
+INSERT 
+INTO issue_status 
+VALUE(19,200701,"returned",7,111);
